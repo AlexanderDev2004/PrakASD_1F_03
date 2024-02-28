c2f1d95a2a7306cec024417c3ff5d01fa9388e5d
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-02-28 12:01:28 +0700

    P2_percobaan2_1F_03_ModifikasiPertayaan

--- P2/systembuku/Buku03.java
+++ P2/systembuku/Buku03.java
@@ -2,25 +2,37 @@ package P2.systembuku;
 
 public class Buku03 {
     String judul, pengarang;
-    String halaman, stock; 
-    String harga; 
+    String halaman, stock,harga ;
+  
 
-    void tampilinformasi (){
-        System.out.println("Judul :" + judul);
-        System.out.println("pengarang :" + pengarang);
-        System.out.println("isi halaman :" + halaman);
-        System.out.println("stock buku :" + stock); 
-        System.out.println("harga Buku :" + harga); 
+    void tampilinformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Isi Halaman: " + halaman);
+        System.out.println("Stok Buku: " + (stock.equals("0") ? "0" : stock));
+        System.out.println("Harga Buku: " + harga);
     }
-    void terjual (int jml){
-        
-        stock = String.valueOf(Integer.parseInt(stock) - jml);
+
+    void terjual(int jml) {
+        int stockInt = Integer.parseInt(stock);
+        if (stockInt >= jml) {
+            stockInt -= jml;
+            stock = String.valueOf(stockInt);
+            System.out.println(jml + " buku telah terjual.");
+        } else {
+            System.out.println("Stok tidak mencukupi untuk penjualan.");
+        }
     }
-    void restok (int jml){
-        
-        stock = String.valueOf(Integer.parseInt(stock) + jml);
+
+    void restok(int jml) {
+        int stockInt = Integer.parseInt(stock);
+        stockInt += jml;
+        stock = String.valueOf(stockInt);
+        System.out.println(jml + " buku telah ditambahkan ke dalam stok.");
     }
-    void gantiharga (int hrg){
+
+    void gantiharga(int hrg) {
         harga = String.valueOf(hrg);
+        System.out.println("Harga buku telah diubah menjadi " + hrg);
     }
 }
