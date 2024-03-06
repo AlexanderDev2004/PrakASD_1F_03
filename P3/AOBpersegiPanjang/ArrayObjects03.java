85fd7f2653ed2ad3f0bf1a67e9a413b1c294ba82
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-03-06 13:09:01 +0700

    P3_Pertayaan1.2_1F_03

--- P3/AOBpersegiPanjang/ArrayObjects03.java
+++ P3/AOBpersegiPanjang/ArrayObjects03.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ArrayObjects03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        PersegiPanjang03[] pparry = new PersegiPanjang03[3];
+        PersegiPanjang03[][] pparry = new PersegiPanjang03[3][3];
         
         // pparry [0] = new PersegiPanjang03();
         // pparry [0].panjang = 110;
@@ -20,17 +20,37 @@ public class ArrayObjects03 {
         // System.out.println("Persegi panjang ke-0, Panjnag : "+ pparry[0].panjang + "Lebar :" + pparry[0].lebar);
         // System.out.println("Persegi panjang ke-0, Panjnag : "+ pparry[1].panjang + "Lebar :" + pparry[1].lebar);
         // System.out.println("Persegi panjang ke-0, Panjnag : "+ pparry[2].panjang + "Lebar :" + pparry[2].lebar);
+        // for (int i = 0; i < 3; i++) {
+        //     pparry [i] = new PersegiPanjang03();
+        //     System.out.println("Persegi panjang ke- " + i);
+        //     System.out.println("Masukan panjang :" );
+        //     pparry[i].panjang = sc.nextInt();
+        //     System.out.println("Masukan lebar :" );
+        //     pparry[i].lebar = sc.nextInt();
+        // }
+        // for (int i = 0; i < 3; i++) {
+        //     System.out.println("Persegi panjang ke- "+ i);
+        //     System.out.println("Panjang :" + pparry[i].panjang + "Lebar : " + pparry[i].lebar);
+        // }
+
         for (int i = 0; i < 3; i++) {
-            pparry [i] = new PersegiPanjang03();
-            System.out.println("Persegi panjang ke- " + i);
-            System.out.println("Masukan panjang :" );
-            pparry[i].panjang = sc.nextInt();
-            System.out.println("Masukan lebar :" );
-            pparry[i].lebar = sc.nextInt();
+            for (int j = 0; j < 3; j++) {
+                pparry[i][j] = new PersegiPanjang03();
+                System.out.println("Persegi panjang ke- " + i + ", " + j);
+                System.out.println("Masukan panjang :");
+                pparry[i][j].panjang = sc.nextInt();
+                System.out.println("Masukan lebar :");
+                pparry[i][j].lebar = sc.nextInt();
+            }
         }
+
+        System.out.println("Data Persegi Panjang:");
+
         for (int i = 0; i < 3; i++) {
-            System.out.println("Persegi panjang ke- "+ i);
-            System.out.println("Panjang :" + pparry[i].panjang + "Lebar : " + pparry[i].lebar);
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Persegi panjang ke- " + i + ", " + j);
+                System.out.println("Panjang: " + pparry[i][j].panjang + ", Lebar: " + pparry[i][j].lebar);
+            }
         }
 
     }    
