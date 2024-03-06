7c9c781444c14203b61c28e6e50bdb2aa945afdb
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-03-06 19:42:21 +0700

    P3_Latihan2_1F_03

--- P3/IpkMhs/IpkMhs03.java
+++ P3/IpkMhs/IpkMhs03.java
@@ -9,8 +9,17 @@ public class IpkMhs03 {
         char[] jenisKelamin = new char[jumlahMahasiswa];
         double[] ipk = new double[jumlahMahasiswa];
 
-        
-        for (int i = 0; i < jumlahMahasiswa; i++) {
+        inputDataMahasiswa(scanner, nama, nim, jenisKelamin, ipk);
+        tampilkanDataMahasiswa(nama, nim, jenisKelamin, ipk);
+
+        double rataRataIPK = hitungRataRataIPK(ipk);
+        System.out.println("Rata-rata IPK: " + rataRataIPK);
+        tampilkanDataMahasiswaTerbesar(nama, nim, jenisKelamin, ipk);
+
+    }
+
+    public static void inputDataMahasiswa(Scanner scanner, String[] nama, String[] nim, char[] jenisKelamin, double[] ipk) {
+        for (int i = 0; i < nama.length; i++) {
             System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
             System.out.print("Masukkan nama : ");
             nama[i] = scanner.nextLine();
@@ -22,17 +31,39 @@ public class IpkMhs03 {
             ipk[i] = scanner.nextDouble();
             scanner.nextLine(); 
         }
+    }
 
-       
-        for (int i = 0; i < jumlahMahasiswa; i++) {
+    public static void tampilkanDataMahasiswa(String[] nama, String[] nim, char[] jenisKelamin, double[] ipk) {
+        for (int i = 0; i < nama.length; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
             System.out.println("Nama : " + nama[i]);
             System.out.println("NIM : " + nim[i]);
             System.out.println("Jenis Kelamin : " + jenisKelamin[i]);
             System.out.println("IPK : " + ipk[i]);
         }
+    }
 
-        scanner.close();
-        
+    public static double hitungRataRataIPK(double[] ipk) {
+        double total = 0;
+        for (double nilai : ipk) {
+            total += nilai;
+        }
+        return total / ipk.length;
+    }
+
+    public static void tampilkanDataMahasiswaTerbesar(String[] nama, String[] nim, char[] jenisKelamin, double[] ipk) {
+        double maxIPK = ipk[0];
+        int indexMaxIPK = 0;
+        for (int i = 1; i < ipk.length; i++) {
+            if (ipk[i] > maxIPK) {
+                maxIPK = ipk[i];
+                indexMaxIPK = i;
+            }
+        }
+        System.out.println("Data Mahasiswa dengan IPK Terbesar:");
+        System.out.println("Nama : " + nama[indexMaxIPK]);
+        System.out.println("NIM : " + nim[indexMaxIPK]);
+        System.out.println("Jenis Kelamin : " + jenisKelamin[indexMaxIPK]);
+        System.out.println("IPK : " + ipk[indexMaxIPK]);
     }
 }
