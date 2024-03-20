c02074a83972e02fbab46a6607fa9e03611b959d
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-03-20 11:08:18 +0700

    P5_Percobaan1_1F_03

---
+++ P5/MainFaktorial03.java
@@ -0,0 +1,27 @@
+package P5;
+import java.util.Scanner;
+public class MainFaktorial03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("======================");
+        System.out.println("Masukan Jumlah Elemet :");
+        int ijml = sc.nextInt();
+
+        Faktorial03[] fk = new Faktorial03[10];
+        for (int i = 0; i < ijml; i++) {
+            fk[i] = new Faktorial03();
+            System.out.println("masukkan nilai data ke-" + (i + 1) + ":");
+            int iNilai = sc.nextInt();
+            fk[i].nilai = iNilai;
+          }
+
+        System.out.println("Hasil-BRUTE Force");
+        for (int i=0; i < ijml; i++) {
+            System.out.println("Hasil perhitungan Faktorial menggunakan Blue Force adalah :" + fk[i].faktorialBF(fk[i].nilai));
+        }
+        System.out.println("Hasil-Divince And conquer");
+        for (int i=0; i < ijml; i++ ) {
+            System.out.println("Hasil perhintungan Faktorial menggunakan Divince And conquer Adalah : "+fk[i].faktorialDC(fk[i].nilai));
+        }
+    }
+}
