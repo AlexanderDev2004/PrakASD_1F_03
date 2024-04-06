110d04feb07016f621ae4e6e32bc0586f177851c
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-04-06 21:07:20 +0700

    Fix code

---
+++ P7/MergeSortTest/MergeSortMain03.java
@@ -0,0 +1,14 @@
+package P7.MergeSortTest;
+
+public class MergeSortMain03 {
+    public static void main(String[] args) {
+        int data [] = {10,40,30,50,70,20,100,90};
+        System.out.println("sorting dan merge Sort ");
+        MergeSorting03 mSort = new MergeSorting03();
+        System.out.println("Data Awal");
+        mSort.printArray(data);
+        mSort.MergeSort(data);
+        System.out.println("Setelah Di urutkan :");
+        mSort.printArray(data);
+    }
+}
