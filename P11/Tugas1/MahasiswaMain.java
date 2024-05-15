1c19b12de4c8f2b12aefa2fb6e960b5d169b3a7b
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-05-16 03:58:07 +0700

    P11_03_AlexanderAgungRaya_Tugas1_1F

---
+++ P11/Tugas1/MahasiswaMain.java
@@ -0,0 +1,14 @@
+package P11.Tugas1;
+
+public class MahasiswaMain {
+    public static void main(String[] args) {
+        SingleLinkedList s03 = new SingleLinkedList();
+        s03.addFirst(111, "Anton");
+        s03.addLast(112, "Prita");
+        s03.addLast(113, "Yusuf");
+        s03.addLast(114, "Doni");
+        s03.addLast(115, "Sari");
+        s03.print();
+
+    }
+}
---
+++ P11/Tugas1/Node.java
@@ -0,0 +1,13 @@
+package P11.Tugas1;
+
+public class Node {
+    int nim;
+    String nama;
+    Node next;
+
+    Node (int nim, String nama){
+        this.nim = nim;
+        this.nama = nama;
+        this.next = null;
+    }
+}
