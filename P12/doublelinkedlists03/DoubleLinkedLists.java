d45c2b3b29cd9319b52fb8760f7747500e978b9d
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-05-18 19:59:21 +0700

    P12_03_AlexanderAgungRaya_percobaan2_1F

--- P12/doublelinkedlists03/DoubleLinkedLists.java
+++ P12/doublelinkedlists03/DoubleLinkedLists.java
@@ -79,4 +79,57 @@ public class DoubleLinkedLists {
             System.out.println("Linked list masih kosong");
         }
     }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, Tidak dapat di hapus");
+        } else if (size == 1) {
+            removeFirst();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+    
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, Tidak dapat di hapus");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+    
+    public void remove (int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Linked list masih kosong, Tidak dapat di hapus");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node current = head;
+            int i =0 ;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
 }
