9f37419d848055161f644594bd278ea27d08bd90
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-06-12 06:22:42 +0700

    P15_03_AlexanderAgungRaya_percobaan1_1F

---
+++ P15/Node03.java
@@ -0,0 +1,12 @@
+public class Node03 {
+    int data;
+    Node03 next, prev;
+    int jarak;
+
+    Node03(Node03 prev, int data,int jarak,Node03 next){
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+        this.jarak = jarak;
+    }
+}
